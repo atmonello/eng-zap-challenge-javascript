74106a92c16ef890d69b444881c1cee9dad75645
Andre Monello <atmonello@outlook.com>
2021-10-08 22:50:08 -0300

    (feat) add first version of home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,10 @@
 <style lang="scss" scoped>
 .theme--dark.v-application {
   background-color: var(--v-background-base, #121212) !important;
+  color: #fff;
 }
 .theme--light.v-application {
   background-color: var(--v-background-base, white) !important;
+  color: #fff;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,56 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col cols="12">Hello</v-col>
-      <v-col cols="12">World</v-col>
+  <v-container class="challenge__home my-16">
+    <v-row class="pt-10">
+      <v-col cols="12" class="d-flex justify-center">
+        <img
+          class="challenge__home__logo--group"
+          src="/logo-grupo.png"
+          alt="Logo do GrupoZap"
+        />
+      </v-col>
+    </v-row>
+    <v-row class="py-6 text-center">
+      <v-col cols="12">
+        <h3 class="text-h3">Teste de engenharia do GrupoZap</h3>
+      </v-col>
+      <v-col cols="12">
+        <h5 class="text-h5">
+          Clique abaixo para filtrar os imóveis por portal
+        </h5>
+      </v-col>
+    </v-row>
+    <v-row class="py-6 text-center">
+      <v-col cols="12" md="6">
+        <v-btn
+          class="
+            rounded-lg
+            pa-6
+            challenge__home__btn challenge__home__btn--viva
+            vivaRealBlue
+          "
+        >
+          <v-img src="/logo-vivareal.png" max-height="36" contain />
+        </v-btn>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-btn
+          class="rounded-lg pa-6 challenge__home__btn challenge__home__btn--zap"
+        >
+          <v-img src="/logo-zap.png" max-height="36" contain />
+        </v-btn>
+      </v-col>
     </v-row>
   </v-container>
 </template>
+
+<style lang="scss" scoped>
+.challenge {
+  &__home {
+    &__logo {
+      &--group {
+        height: 48px;
+      }
+    }
+  }
+}
+</style>
