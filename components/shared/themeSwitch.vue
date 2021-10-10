1d5f7d33c12a10e215acb46e3467567f619392ec
Andre Monello <atmonello@outlook.com>
2021-10-10 13:07:59 -0300

    (refactor) add div container to theme-switcher component

--- components/shared/themeSwitch.vue
+++ components/shared/themeSwitch.vue
@@ -1,23 +1,25 @@
 <template>
-  <v-input dense hide-details class="d-flex align-center">
-    <template v-slot:prepend class="ma-0">
-      <v-icon class="theme__switch__icon--prepend"
-        >mdi-white-balance-sunny</v-icon
-      >
-    </template>
-    <v-switch
-      class="pa-0 ma-0"
-      inset
-      prepend-icon=""
-      hide-details
-      slot="default"
-      :value="isDark"
-      @change="changeTheme"
-    ></v-switch>
-    <template v-slot:append>
-      <v-icon class="theme__switch__icon--append">mdi-weather-night</v-icon>
-    </template>
-  </v-input>
+  <div class="d-block">
+    <v-input dense hide-details class="theme__switch d-flex align-center">
+      <template v-slot:prepend class="ma-0">
+        <v-icon class="theme__switch__icon--prepend"
+          >mdi-white-balance-sunny</v-icon
+        >
+      </template>
+      <v-switch
+        class="pa-0 ma-0"
+        inset
+        prepend-icon=""
+        hide-details
+        slot="default"
+        :value="isDark"
+        @change="changeTheme"
+      ></v-switch>
+      <template v-slot:append>
+        <v-icon class="theme__switch__icon--append">mdi-weather-night</v-icon>
+      </template>
+    </v-input>
+  </div>
 </template>
 
 <style lang="scss">
