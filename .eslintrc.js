e750bef0f9b20db9e4e05e74bcbb45cc50348aff
Andre Monello <atmonello@outlook.com>
2021-10-10 15:24:53 -0300

    (chore) remove prettier from project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,27 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:prettier/recommended",
-        "plugin:nuxt/recommended",
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:nuxt/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  ignorePatterns: ["*.config.js", ".eslintrc.js", "test/*"],
+  rules: {
+    indent: ["error", 2],
+    "no-console": 2,
+    quotes: [
+      "error",
+      "double",
+      {
+        allowTemplateLiterals: true,
+        avoidEscape: true,
+      },
     ],
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: "module",
-    },
-    plugins: ["prettier"],
-    ignorePatterns: [
-        "*.config.js",
-        ".eslintrc.js",
-        "test/*"
-    ],
-    rules: {
-        indent: ["error", 2],
-        "nuxt-no-cjs-in-config": "off",
-        "no-console": 2,
-        quotes: [
-            "error",
-            "double",
-            {
-                allowTemplateLiterals: true,
-                avoidEscape: true,
-            },
-        ],
-        semi: ["error", "always"],
-        "comma-dangle": ["error", "only-multiline"],
-    },
+    semi: ["error", "always"],
+    "comma-dangle": ["error", "only-multiline"],
+    "prettier/prettier": ["off"],
+  },
 };
