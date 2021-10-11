bb093056d6a0cfc1032f6abca6595ade19290b6c
Andre Monello <atmonello@outlook.com>
2021-10-10 21:39:53 -0300

    (feat) implement property save to localstorage

--- components/property/detail.vue
+++ components/property/detail.vue
@@ -5,12 +5,11 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
 export default {
-  computed: {
-    ...mapGetters("properties", ["listPropertiesById"]),
-    selectedProperty() {
-      return this.listPropertiesById[this.$route.params.property];
+  props: {
+    selectedProperty: {
+      type: Object,
+      required: true,
     },
   },
 };
--- pages/portals/_portal/properties/_property.vue
+++ pages/portals/_portal/properties/_property.vue
@@ -1,15 +1,44 @@
 <template>
   <v-container>
-    <property-detail></property-detail>
+    <property-detail :selected-property="selectedProperty"></property-detail>
   </v-container>
 </template>
 
 <script>
+/* eslint-disable */
+import { mapGetters } from "vuex";
 import PropertyDetail from "@/components/property/detail.vue";
 export default {
   layout: "properties",
   components: {
     PropertyDetail,
+  },
+  data() {
+    return {
+      storedProperty: {},
+    };
+  },
+  computed: {
+    ...mapGetters("properties", ["listPropertiesById"]),
+    selectedProperty() {
+      return (
+        this.listPropertiesById[this.$route.params.property] ||
+        this.storedProperty ||
+        {}
+      );
+    },
+  },
+  created() {
+    if (!!this.selectedProperty.id && process.client) {
+      localStorage.setItem(
+        "SelectedPropertyInfo",
+        JSON.stringify(this.selectedProperty)
+      );
+    } else {
+      this.storedProperty = JSON.parse(
+        localStorage.getItem("SelectedPropertyInfo")
+      );
+    }
   },
 };
 </script>
