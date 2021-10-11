68096e236fad9154191c358da9a7f20ba8c06a0e
Andre Monello <atmonello@outlook.com>
2021-10-10 21:06:08 -0300

    (style) change property list item design

--- components/properties/listItem.vue
+++ components/properties/listItem.vue
@@ -11,27 +11,33 @@
       ></v-carousel-item>
     </v-carousel>
     <v-card :to="property.id" nuxt append>
-      <v-card-title>
-        <span class="font-weight-bold">{{
-          formatPrice(this.property.pricingInfos.price)
-        }}</span>
-        <span v-if="isRentalProperty" class="font-weight-regular"> / mês</span>
-        <span v-else class="font-weight-regular"> / venda</span>
-      </v-card-title>
-      <v-card-subtitle>
-        <p>
+      <v-card-text>
+        <span class="text-body-1 font-weight-regular">
           Imóvel em {{ property.address.neighborhood }},
           {{ property.address.city }}
+        </span>
+        <p class="mt-1 mb-3">
+          <span class="text-h5 font-weight-black">
+            {{ formatPrice(this.property.pricingInfos.price) }}
+          </span>
+          <span v-if="isRentalProperty" class="font-weight-regular">
+            / mês</span
+          >
+          <span v-else class="font-weight-regular"> / venda</span>
         </p>
-        <span class="mr-2">{{ property.usableAreas }} m²</span>
-        <span class="mr-2"
+        <span class="font-weight-black text-body mr-2"
+          >{{ property.usableAreas }} m²</span
+        >
+        <span class="font-weight-black text-body mr-2"
           ><v-icon>mdi-bed</v-icon> {{ property.bedrooms }}</span
         >
-        <span class="mr-2"
+        <span class="font-weight-black text-body mr-2"
           ><v-icon>mdi-toilet</v-icon> {{ property.bathrooms }}</span
         >
-        <span><v-icon>mdi-car</v-icon> {{ property.parkingSpaces }}</span>
-      </v-card-subtitle>
+        <span class="font-weight-black text-body"
+          ><v-icon>mdi-car</v-icon> {{ property.parkingSpaces }}</span
+        >
+      </v-card-text>
     </v-card>
   </v-card>
 </template>
