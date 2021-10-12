182ec13e11f74bcca413d580fc63d123b492d4c7
Andre Monello <atmonello@outlook.com>
2021-10-12 15:44:59 -0300

    (refactor) update detailed features and finance components

--- components/property/detailedFeatures.vue
+++ components/property/detailedFeatures.vue
@@ -1,26 +1,39 @@
 <template>
-  <div>
-    <h6 class="grey--text text--darken-1 font-weight-medium text-caption">
-      <v-icon class="mr-2" :size="iconSize">mdi-set-square</v-icon>
-      {{ area }} m²
-    </h6>
-    <h6 class="grey--text text--darken-1 font-weight-medium text-caption">
-      <v-icon class="mr-2" :size="iconSize">mdi-bed</v-icon>
-      {{ bedrooms }} quartos
-    </h6>
-    <h6 class="grey--text text--darken-1 font-weight-medium text-caption">
-      <v-icon class="mr-2" :size="iconSize">mdi-toilet</v-icon>
-      {{ bathrooms }} banheiros
-    </h6>
-    <h6
-      class="grey--text text--darken-1 font-weight-medium text-caption"
+  <div class="d-sm-flex">
+    <p
+      class="grey--text pr-sm-4 text--darken-1 font-weight-medium text-caption"
+    >
+      <v-icon class="mr-2 mr-sm-1" :size="iconSize">mdi-set-square</v-icon
+      >{{ area }} m²
+    </p>
+    <p
+      class="grey--text pr-sm-4 text--darken-1 font-weight-medium text-caption"
+    >
+      <v-icon class="mr-2 mr-sm-1" :size="iconSize">mdi-bed</v-icon
+      >{{ bedrooms }} quartos
+    </p>
+    <p
+      class="grey--text pr-sm-4 text--darken-1 font-weight-medium text-caption"
+    >
+      <v-icon class="mr-2 mr-sm-1" :size="iconSize">mdi-toilet</v-icon
+      >{{ bathrooms }} banheiros
+    </p>
+    <p
+      class="grey--text pr-sm-4 text--darken-1 font-weight-medium text-caption"
       v-if="parking > 0"
     >
-      <v-icon class="mr-2" :size="iconSize">mdi-car</v-icon>
-      {{ parking }} {{ parking === 1 ? "vaga" : "vagas" }}
-    </h6>
+      <v-icon class="mr-2 mr-sm-1" :size="iconSize">mdi-car</v-icon
+      >{{ parking }}
+      {{ parking === 1 ? "vaga" : "vagas" }}
+    </p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+p {
+  margin-bottom: 0;
+}
+</style>
 
 <script>
 export default {
--- components/property/detailedFinance.vue
+++ components/property/detailedFinance.vue
@@ -1,31 +1,54 @@
 <template>
-  <div class="d-flex flex-column px-6">
+  <div class="d-flex flex-column mt-2 px-6 px-sm-0">
     <div class="d-flex justify-space-between">
-      <p class="text-subtitle-2 font-weight-bold">
+      <p class="text-subtitle-2 font-weight-bold d-block d-sm-none">
         {{ priceInfo.businessType === "SALE" ? "Valor" : "Aluguel" }}
       </p>
-      <p class="text-xl-h1 text-subtitle-1 font-weight-bold">
+      <p class="text-subtitle-1 text-sm-h2 text-xl-h1 font-weight-bold">
         {{ formatPrice(priceInfo.price) }}
+        <span
+          class="d-none d-sm-inline-block text-h4"
+          v-if="priceInfo.businessType === 'RENTAL'"
+        >
+          / mês</span
+        >
       </p>
     </div>
-    <div class="d-flex justify-space-between">
-      <p class="text-subtitle-2">Condomínio</p>
-      <p class="text-xl-h1 text-subtitle-1">
-        {{ formatPrice(priceInfo.monthlyCondoFee) }}
-      </p>
-    </div>
-    <div class="d-flex justify-space-between">
-      <p class="text-subtitle-2">IPTU</p>
-      <p class="text-xl-h1 text-subtitle-1">
-        {{ formatPrice(priceInfo.yearlyIptu) }}
-      </p>
+    <div class="d-flex flex-column flex-sm-row justify-sm-start">
+      <div
+        class="
+          d-flex
+          justify-space-between justify-sm-start
+          align-sm-center
+          mr-sm-6
+        "
+      >
+        <p class="text-subtitle-2 pr-sm-2">Condomínio</p>
+        <p class="d-sm-inline-block text-xl-h1 text-subtitle-1">
+          <span v-if="Number(priceInfo.monthlyCondoFee)">
+            {{ formatPrice(priceInfo.monthlyCondoFee) }}
+          </span>
+          <span v-else>não informado</span>
+        </p>
+      </div>
+      <div
+        class="d-flex justify-space-between justify-sm-start align-sm-center"
+      >
+        <p class="text-subtitle-2 pr-sm-2">IPTU</p>
+        <p class="d-sm-inline-block text-xl-h1 text-subtitle-1">
+          <span v-if="Number(priceInfo.yearlyIptu)">
+            {{ formatPrice(priceInfo.yearlyIptu) }}
+          </span>
+          <span v-else>não informado</span>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 p {
-  margin-bottom: 8px;
+  margin-bottom: 0;
 }
 </style>
 
@@ -42,7 +65,7 @@ export default {
   },
   methods: {
     formatPrice(val) {
-      return formatPrice(val);
+      return formatPrice(val, 0);
     },
   },
 };
